ac64427f69cc90d00a891fe87b5ceb773efba25a
shubhamyadav97660@gmail.com <neeta6854@gmail.com>
2024-03-25 18:09:38 +0530

    third commit

--- smoothscroll.js
+++ smoothscroll.js
@@ -1,13 +1,15 @@
 const scroll = new LocomotiveScroll({
-  el: document.querySelector('#main'), // id
-  smooth: true
+  el: document.querySelector("#main"), // id
+  smooth: true,
 });
 
-function mincircleMouseFollower () {
-  window.addEventListener("mousemove", function(dets) {
+function mincircleMouseFollower() {
+  $(document.body).on("mousemove", function (dets) {
     // console.log(dets.clientX, dets.clientY);
-    this.document.querySelector("#mincircle").style.transform = `translate(${dets.clientX}px, ${dets.clientY}px)`
-  })
+    $("#mincircle").css({
+      transform: `translate(${dets.clientX}px, ${dets.clientY}px)`,
+    });
+  });
 }
 
 mincircleMouseFollower();
